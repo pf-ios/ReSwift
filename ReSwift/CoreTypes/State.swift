aa2b45685dad7f12b528922873f0478b54f8b292
Sean Park <sean.2015@kakaocorp.com>
2017-11-30 10:56:09 +0900

    Markable Protocol Equatable Update

--- ReSwift/CoreTypes/State.swift
+++ ReSwift/CoreTypes/State.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public protocol StateType { }
-public protocol Markable {
+public protocol Markable: Equatable {
     var updatedAt: Date { get set }
     var isUpdated: Bool { get set }
     func setUpdated()
@@ -20,9 +20,7 @@ extension Markable {
         isUpdated = updated
         updatedAt = Date()
     }
-}
-
-extension Markable: Equatable {
+    
     static func == (lhs: Self, rhs: Self) -> Bool {
         return lhs.updatedAt == rhs.updatedAt
     }
