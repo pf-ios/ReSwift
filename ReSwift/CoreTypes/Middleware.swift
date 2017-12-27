a88ca8a51f4b806f948308699f907de24e25e574
Sean Park <sean.2015@kakaocorp.com>
2017-12-27 17:05:43 +0900

    DispatchFunction returning Again

--- ReSwift/CoreTypes/Middleware.swift
+++ ReSwift/CoreTypes/Middleware.swift
@@ -8,6 +8,6 @@
 
 import Foundation
 
-public typealias DispatchFunction = (Action) -> Void
+public typealias DispatchFunction = (Action) -> Any
 public typealias Middleware<State> = (@escaping DispatchFunction, @escaping () -> State?)
     -> (@escaping DispatchFunction) -> DispatchFunction
